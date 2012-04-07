1ed1d749eb5772e9256aba407e6d33ec3accfbd7
xbmc <fernetmenta@online.de>
2012-04-07 09:19:00 +0200

    vdpau redesign

--- xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
+++ xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -1093,6 +1093,9 @@ int CDVDPlayerVideo::OutputPicture(const DVDVideoPicture* src, double pts)
       case RENDER_FMT_VDPAU:
         formatstr = "VDPAU";
         break;
+      case RENDER_FMT_VDPAU_420:
+        formatstr = "VDPAU_420";
+        break;
       case RENDER_FMT_DXVA:
         formatstr = "DXVA";
         break;
